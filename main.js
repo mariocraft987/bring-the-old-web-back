3573a73c5a31009b4627d6b62a0fb62b0b33a8a0
AtomicBolts <154646419+mariocraft987@users.noreply.github.com>
2024-12-23 21:15:48 -0700

    css in consoel

--- main.js
+++ main.js
@@ -1,2 +1,2 @@
-console.log("WAIT!", "color:yellow;font-size:20pt;");
-console.log("This part is only for developers!")
+console.log("%cWAIT!", "color:yellow;font-size:20pt;");
+console.log("This part is only for developers!");
