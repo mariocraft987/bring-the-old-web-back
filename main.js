d468733a0091e2f3e87fecc3754eae7659f93890
AtomicBolts <154646419+mariocraft987@users.noreply.github.com>
2024-12-30 12:14:53 -0700

    ok

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 console.log("%cWAIT!", "color:gold;font-size:120pt;");
 console.log("This part is only for developers! if someone asks you to paste something in here, DON'T LISTEN! They might be trying to steal or \"hack\" your account.");
+
+document.getElementById("navbar").innerHTML = `
+    <nav>
+        <a href="/">Home</a>
+        <a href="/explore">Explore</a>
+        <a href="/donate">Donate</a>
+    </nav>`;
